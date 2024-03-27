da53d88e19ae3f42797756696fca48db30f77db1
Erneypuetate <erney336@gmail.com>
2024-03-27 19:28:10 +0000

    hola mundo

--- config/db.py
+++ config/db.py
@@ -1,3 +1,3 @@
 from pymongo import MongoClient
 
-conn = MongoClient("mongodb://root:example@localhost:27017/")
+conn = MongoClient("mongodb://root:example@mongo:27017/")
--- routers/user.py
+++ routers/user.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter, Response, status, HTTPException
+from fastapi import APIRouter, Response, status, HTTPException, Request, Form
+from fastapi.templating import Jinja2Templates
 from config.db import conn
 from schemas. user import userentity, usersentity
 from models.user import User
@@ -8,56 +9,59 @@ from starlette.status import HTTP_204_NO_CONTENT
 
 user=APIRouter()
 
+templates = Jinja2Templates(directory="views")
 
-@user.post('/users')
-def create_user(user: User):
-    try:
-        print("1")
-        new_user = dict(user)
-        print(new_user)
-        conn.erney.second_db_in_mongo.insert_one(new_user)
-        return new_user
-    except Exception as e:
-        # Manejo de excepciones
-        print(f"Error al insertar el usuario en la base de datos: {e}")
-        raise HTTPException(status_code=500, detail="Error interno del servidor")
+@user.get("/")
+async def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
 
-#@user.get('/users', response_model=list[User], tags=["users"])
-#def fidn_all_user():
- #   return usersentity(conn.erney.second_db_in_mongo.find())
+@user.post("/login")
+async def login(username: str = Form(...), password: str = Form(...)):
+    # Aquí puedes agregar la lógica para autenticar al usuario con los datos recibidos
+    return {"username": username, "password": password}
+
+@user.get('/')
+def login():
+    return "estamos trabajando en el login"
+
+
+
+@user.get('/users', response_model=list[User], tags=["users"])
+def fidn_all_user():
+    return usersentity(conn.erney.second_db_in_mongo.find())
 
 
    
-#@user.post('/users', response_model=User, tags=["users"])
-#def create_user(user:User):
-    #new_user = dict(user)
-    #new_user["password"]=sha256_crypt.encrypt(new_user["password"])
+@user.post('/users', response_model=User, tags=["users"])
+def create_user(user:User):
+    new_user = dict(user)
+    new_user["password"]=sha256_crypt.encrypt(new_user["password"])
 
-    #id= conn.erney.second_db_in_mongo.insert_one(new_user).inserted_id
+    id= conn.erney.second_db_in_mongo.insert_one(new_user).inserted_id
 
-    #user=conn.erney.second_db_in_mongo.find_one({"_id":id})
+    user=conn.erney.second_db_in_mongo.find_one({"_id":id})
 
-    #return userentity(user)
-
-
-
-#@user.get('/users/{id}', response_model=User, tags=["users"])
-#def find_user(id:str):
-  ##  return userentity(conn.erney.second_db_in_mongo.find_one({"_id":ObjectId(id)}))
+    return userentity(user)
 
 
 
-#@user.put('/users/{id}', response_model=User, tags=["users"])
-#def update_user(id:str, user:User):
-    #conn.erney.second_db_in_mongo.find_one_and_update({"_id":ObjectId(id)}, {"$set":dict(user)})
+@user.get('/users/{id}', response_model=User, tags=["users"])
+def find_user(id:str):
+    return userentity(conn.erney.second_db_in_mongo.find_one({"_id":ObjectId(id)}))
+
+
+
+@user.put('/users/{id}', response_model=User, tags=["users"])
+def update_user(id:str, user:User):
+    conn.erney.second_db_in_mongo.find_one_and_update({"_id":ObjectId(id)}, {"$set":dict(user)})
     
-    #return userentity(conn.erney.second_db_in_mongo.find_one({"_id":ObjectId(id)}))
+    return userentity(conn.erney.second_db_in_mongo.find_one({"_id":ObjectId(id)}))
 
 
 
-#@user.delete('/users/{id}', status_code=HTTP_204_NO_CONTENT, tags=["users"])
-#def delate_user(id:str):
- #   conn.erney.second_db_in_mongo.find_one_and_delete({"_id":ObjectId(id)})
+@user.delete('/users/{id}', status_code=HTTP_204_NO_CONTENT, tags=["users"])
+def delate_user(id:str):
+    conn.erney.second_db_in_mongo.find_one_and_delete({"_id":ObjectId(id)})
   
-  #  return Response(status_code=HTTP_204_NO_CONTENT)
+    return Response(status_code=HTTP_204_NO_CONTENT)
